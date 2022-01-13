6a5fe4f29ec57a7f170aa406fef4dac391c956a7
treki <tuya.senff@gmail.com>
2022-01-13 12:24:41 -0600

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="block" v-if="showBlock">
+      Click Me
+      </div>
+</template>
+
+<script>
+export default {
+props: ['delay'],
+data(){
+    return{
+        showBlock: false
+    }
+},
+mounted(){
+    console.log('component mounted')
+    setTimeout(()=>{
+        this.showBlock = true
+        console.log(this.delay)
+    }, this.delay)
+},
+updated(){
+
+}
+}
+</script>
+
+<style>
+.block{
+    width: 250px;
+    height: 250px;
+    border-radius: 4px;
+    background: rgb(66, 199, 86);
+    text-align: 100px 50px;
+    margin: 40px auto;
+}
+</style>
